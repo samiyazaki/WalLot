2809767980e4fe99e6e83ee7a3164e2b106ab4ba
Seiya Miyazaki <smsmiyazaki@gmail.com>
2023-02-25 18:19:01 -0500

    uninstalling deprecated npm dependencies

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const { Finance } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET route to retrieve user's financial data
-router.get('/data', (req, res) => {
-  // logic to retrieve and return user's financial data
+router.get('/data', withAuth, async (req, res) => {
+  try {
+    const financeData = await Finance.findAll({
+      where: {
+        user_id: req.session.user_id,
+      },// logic to retrieve and return user's financial data
 });
 
+res.status(200).json(financeData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
 // POST route to add new financial data
-router.post('/data', (req, res) => {
+router.post('/data', withAuth, async (req, res) => {
+  try {
+    const newFinance = await Finance.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newFinance);
+  } catch (err) {
+    res.status(400).json(err);
+  } 
   // logic to add new financial data to the database
 });
 
