d8d76b17e4726560ae64c0151d39b5624b56be22
Savita Rankavat <savita@Savitas-MacBook-Pro.local>
2023-02-28 12:22:11 -0600

    added routes

---
+++ controllers/api/incomeRoutes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { Income } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newIncome = await Income.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newIncome);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
