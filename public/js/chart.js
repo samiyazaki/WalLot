1b57dad7283cd0703679a386b2c4dab85a0e515f
mkaye <mattkaye97@gmail.com>
2023-03-02 21:22:09 -0500

    Enabled chart functionality to pull from data already on the page. Added variables which parse the data to remove commas.

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,20 +1,21 @@
 const ctx = document.getElementById('myChart');
-const cv1 = document.getElementById('need-income').textContent;
-const cv2 = document.getElementById('wants').textContent;
-const cv3 = document.getElementById('savings').textContent;
-const cv4 = document.getElementById('savings').textContent;
+
+const cv1 = document.getElementById('wants').textContent.split(',').join('');
+const cv2 = document.getElementById('needs').textContent.split(',').join('');
+const cv3 = document.getElementById('savings').textContent.split(',').join('');
+const cv4 = .5*cv3;
 
 // Remove once pulling data from database
-const dummyData = [10, 10, 50, 30];
+// const dummyData = [10, 10, 50, 30];
 const dbData = [cv1, cv2, cv3, cv4];
 
 new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Goal Savings', 'General Savings', 'Needs', 'Wants'],
+        labels: ['Wants', 'Needs', 'Total Savings', 'Goal Savings'],
         datasets: [{
-            label: 'Relative Income Distribution',
-            data: dummyData,
+            label: 'Relative Income Distribution (in $)',
+            data: dbData,
             borderWidth: 10
         }]
     },
