145553c53004cb15d7f24696613bb8d24e41f6e0
mkaye <mattkaye97@gmail.com>
2023-03-02 19:31:11 -0500

    Chart now renders on plans body using chart.js file. Corrected CDN for chart.js.

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,22 +1,23 @@
 const ctx = document.getElementById('myChart');
-
+// Remove once pulling data from database
+const dummyData = [10, 10, 50, 30];
+const dbData = ['goal savings amount', 'general savings amount', 'Needs amount', 'Wants Amount'];
 new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+    type: 'bar',
+    data: {
+        labels: ['Goal Savings', 'General Savings', 'Needs', 'Wants'],
+        datasets: [{
+            label: 'Relative Income Distribution',
+            data: dummyData,
+            borderWidth: 10
+        }]
+    },
+    options: {
+        backgroundColor: ['red', 'green', 'yellow', 'pink'],
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
-  }
 });
-
-ctx.append(theChart);
